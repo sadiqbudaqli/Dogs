8cf7328cf5bbac6ea124ee2fb1d5a8c874d3f521
sadiqbudaqli <sadiq.budaqli@gmail.com>
2020-06-26 17:06:56 +0200

    AI-4.0 <sadiq@LAPTOP-EOURGQIS Overwrite remote C:\Users\sadiq\AndroidStudioProjects\Dogs2 to local

--- app/src/main/java/com/example/dogs/model/DogsApi.kt
+++
@@ -1,9 +0,0 @@
-package com.example.dogs.model
-
-import io.reactivex.Single
-import retrofit2.http.GET
-
-interface DogsApi {
-    @GET("DevTides/DogsApi/master/dogs.json")
-    fun getDogDogs(): Single<List<DogBreed>>
-}
--- app/src/main/java/com/example/dogs/view/DogClickListener.kt
+++
@@ -1,7 +0,0 @@
-package com.example.dogs.view
-
-import android.view.View
-
-interface DogClickListener {
-    fun onDogClicked(v: View)
-}
